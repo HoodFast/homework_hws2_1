233ae908b77d368f11136b5226096da13d409118
HoodFast <rabota-trassa@mail.ru>
2023-03-15 12:33:58 +0300

    commit

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HW6 from "../../hw06/HW6";
 import HW7 from '../../hw07/HW7'
 import HW8 from "../../hw08/HW8";
+import HW9 from "../../hw09/HW9";
 // import HW8 from '../../hw08/HW8'
 // import HW9 from '../../hw09/HW9'
 
@@ -11,7 +12,7 @@ function Junior() {
             <HW6 />
             <HW7 />
             <HW8 />
-            {/*<HW9 />*/}
+            <HW9 />
         </div>
     )
 }
