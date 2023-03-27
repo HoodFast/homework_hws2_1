89a795a8697ee45b76ce7cf66ea3dcf530ed8652
HoodFast <rabota-trassa@mail.ru>
2023-03-27 16:05:01 +0300

    commit

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
@@ -13,9 +13,23 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+    const [value2, setValue2] = useState(restoreState<number[]>('hw11-value2', [0, 100]))
 
-    const change = (event: any, value: any) => {
+    const change = (event: any, value: any, activeThumb: number) => {
+        if (event.target.name === 'hw11-single-slider' ) {
+            setValue1(value)
+            setValue2([value,value2[1]] )
+        } else {
+            setValue2(value)
+        }
+        // const minDistance = 5
+        // if (activeThumb === 0) {
+        //     setValue2([Math.min(value[0], value2[1] - minDistance), value2[1]]);
+        // } else {
+        //     setValue2([value2[0], Math.max(value[1], value2[0] + minDistance)]);
+        // }
+
+
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
     }
 
@@ -29,6 +43,9 @@ function HW11() {
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
+                            value={value1}
+                            onChange={change}
+                            name={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
 
                         />
@@ -37,10 +54,13 @@ function HW11() {
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
+                            value={value2}
+                            onChange={change}
+                            name={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
 
                         />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+                        <span id={'hw11-value-2'} className={s.number}>{`${value2[0]} - ${value2[1]}`}</span>
                     </div>
                 </div>
             </div>
