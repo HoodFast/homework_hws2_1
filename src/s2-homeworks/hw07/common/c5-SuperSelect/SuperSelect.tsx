55afc339fb2b17211c24bcc0f9744c6183e01485
HoodFast <rabota-trassa@mail.ru>
2023-03-27 17:30:06 +0300

    commit

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -36,7 +36,8 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
         : [] // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        onChangeOption && onChangeOption(e.currentTarget.value)
+        onChange?.(e);
+        onChangeOption?.(+e.currentTarget.value);
         // делают студенты
     }
 
