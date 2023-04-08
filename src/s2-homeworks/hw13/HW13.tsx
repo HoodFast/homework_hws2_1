a0826d984017f88a15d08b43ab8670c044897a81
HoodFast <rabota-trassa@mail.ru>
2023-04-09 00:18:49 +0300

    commit

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -7,6 +7,7 @@ import success200 from './images/200.svg'
 import error400 from './images/400.svg'
 import error500 from './images/500.svg'
 import errorUnknown from './images/error.svg'
+import {Loader} from "../hw10/Loader";
 
 /*
 * 1 - дописать функцию send
@@ -19,7 +20,7 @@ const HW13 = () => {
     const [text, setText] = useState('')
     const [info, setInfo] = useState('')
     const [image, setImage] = useState('')
-    const [Load, setLoad] = useState(false)
+    const [isLoad, setIsLoad] = useState(false)
 
     const send = (x?: boolean | null) => () => {
         const url =
@@ -31,7 +32,7 @@ const HW13 = () => {
         setImage('')
         setText('')
         setInfo('...loading')
-        setLoad(true)
+        setIsLoad(true)
         axios
             .post(url, {success: x})
             .then((res) => {
@@ -42,28 +43,28 @@ const HW13 = () => {
 
             })
             .catch((e) => {
-                // дописать
-                console.log(e)
-                if (e.response.data) {
-                    setText(e.response.data.errorText)
-                    setInfo(e.response.data.info)
-                } else {
-                    setCode('Error!')
-                    setImage(errorUnknown)
-                    setText("Network Error")
-                    setInfo("AxiosError")
-                }
-
                 if (e.response.status === 400) {
                     setCode('Ошибка 400!')
                     setImage(error400)
-                } else if (e.response.status === 500) {
-                    setCode('Ошибка 500!')
-                    setImage(error500)
+                    setText('Ты не отправил success в body вообще!')
+                    setInfo('ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
                 }
-            }).finally(()=>{
-                setLoad(false)
-        })
+                if (e.response.status === 500) {
+                    setImage(error500)
+                    setCode('Ошибка 500!')
+                    setText('эмитация ошибки на сервере')
+                    setInfo('ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
+                }
+                if (e.code === "ERR_NETWORK") {
+                    setImage(errorUnknown)
+                    setCode('Error!')
+                    setInfo('Network Error AxiosError')
+                }
+
+            })
+            .finally(() => {
+                setIsLoad(false)
+            })
     }
 
     return (
@@ -76,8 +77,7 @@ const HW13 = () => {
                         id={'hw13-send-true'}
                         onClick={send(true)}
                         xType={'secondary'}
-                        // дописать
-                        disabled={Load}
+                        disabled={isLoad}
 
                     >
                         Send true
@@ -86,8 +86,8 @@ const HW13 = () => {
                         id={'hw13-send-false'}
                         onClick={send(false)}
                         xType={'secondary'}
-                        // дописать
-                        disabled={Load}
+                        disabled={isLoad}
+
                     >
                         Send false
                     </SuperButton>
@@ -95,8 +95,8 @@ const HW13 = () => {
                         id={'hw13-send-undefined'}
                         onClick={send(undefined)}
                         xType={'secondary'}
-                        // дописать
-                        disabled={Load}
+                        disabled={isLoad}
+
                     >
                         Send undefined
                     </SuperButton>
@@ -104,35 +104,36 @@ const HW13 = () => {
                         id={'hw13-send-null'}
                         onClick={send(null)} // имитация запроса на не корректный адрес
                         xType={'secondary'}
-                        // дописать
-                        disabled={Load}
+                        disabled={isLoad}
+
                     >
                         Send null
                     </SuperButton>
                 </div>
 
-                <div className={s.responseContainer}>
-                    <div className={s.imageContainer}>
-                        {image && <img src={image} className={s.image} alt="status"/>}
-                    </div>
+                {isLoad
+                    ? <Loader/>
+                    : <div className={s.responseContainer}>
+                        <div className={s.imageContainer}>
+                            {image && <img src={image} className={s.image} alt="status"/>}
+                        </div>
 
-                    <div className={s.textContainer}>
-                        <div id={'hw13-code'} className={s.code}>
-                            {code}
-                        </div>
-                        <div id={'hw13-text'} className={s.text}>
-                            {text}
-                        </div>
-                        <div id={'hw13-info'} className={s.info}>
-                            {info}
+                        <div className={s.textContainer}>
+                            <div id={'hw13-code'} className={s.code}>
+                                {code}
+                            </div>
+                            <div id={'hw13-text'} className={s.text}>
+                                {text}
+                            </div>
+                            <div id={'hw13-info'} className={s.info}>
+                                {info}
+                            </div>
                         </div>
                     </div>
-                </div>
+                }
             </div>
         </div>
     )
 }
 
 export default HW13
-
-
